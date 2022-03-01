3a6b6a05c59d1aeaa14e5abdf25bf8e422b127dd
Shiggy <ilarumbgim1@educacion.navarra.es>
2022-03-01 19:06:37 +0100

    feat: first commit

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<svelte:head>
+  <title>Castilla</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+</svelte:head>
+<div class="text-white bg-gray-800 h-full w-full absolute">
+  <div class="md:mx-auto m-2 md:container mx-auto bg-zinc-800 border-b-4 border-b-rose-700">
+    <div class="p-2 md:flex mx-auto md:text-left text-center justify-items-center items-center justify-evenly space-x-2">
+      <div class="text-xl font-mono">Castilla <span class="bg-gray-300 inline-block font-mono p-0.5 rounded text-gray-500">v1.0</span></div>
+    </div>
+  </div>
+  <div class="container mx-auto bg-zinc-800 p-1 ">
+    <h1 class="text-2xl p-1 text-center">The FOSS solution for your community.</h1>
+    <div class="lg:w-2/4 mx-auto bg-zinc-900 p-2 rounded">
+      <p class="p-1 text-lg font-mono bg-zinc-700">Castilla, from a sandbox to a service.</p>
+      <p class="p-1 whitespace-pre-line">Castilla, originally was a sandbox website like many others, except it stood out for the impressive uptime on it's limited conditions, and friendly community.
+
+        But after almost two years, it shut down for a major good, the personal growth of the owners.
+
+        And now a year later, the former <span class="underline">Castilla team along with Anomia</span> have worked to release Castilla <span class="bg-zinc-700 p-0.5 items-center rounded text-sm">V1.0</span>, the Open-Source version of Castilla. For everyone to use freely.
+      </p>
+    </div>
+  </div>
+  <div class="container mx-auto bg-zinc-800 p-1 border-b-4 text-center border-b-rose-700">
+    <h1 class="text-2xl p-1 text-center">FAQ</h1>
+    <div class="lg:w-2/4 mx-auto grid md:grid-cols-2 gap-2 bg-zinc-900 p-2 rounded">
+      <div class="bg-zinc-800 p-2 rounded shadow">
+        <div class="text-center mb-1"><li class="fa fa-question text-2xl "></li></div>
+        <p>Who is developing this?</p>
+        <p class="bg-rose-900 m-1  rounded p-1">Castilla developer & maintainer Gatari, aka Edge, aswell as many other open source contributors.</p>
+      </div>
+                <div class="bg-zinc-800 p-2 rounded shadow">
+        <div class="text-center mb-1"><li class="fa fa-question text-2xl "></li></div>
+        <p>Why is it open source?</p>
+        <p class="bg-rose-900 m-1 rounded p-1">We believe the sandbox platforms needs to expand further, and that includes giving everyone access to create their own secure yet fun platforms.</p>
+      </div>          <div class="bg-zinc-800 p-2 rounded col-span-2 shadow">
+        <div class="text-center mb-1"><li class="fa fa-question text-2xl "></li></div>
+        <p>Is it completely free to use?</p>
+        <p class="bg-rose-900 m-1 rounded p-1">In basic words, yes. But there's a catch. Castilla is released under a special license that conditions the owners to at least, credit the developers of this project in any instance hosted, and to not commercialize this project for security reasons.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  :root {
+    --tw-bg-opacity: 1;
+    background-color: rgb(31 41 55 / var(--tw-bg-opacity));
+}
+</style>
